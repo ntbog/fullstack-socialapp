69ef537d2a36c24fc0ac001954cb4f291bb8fe72
ntbog <wakasaga@gmail.com>
2020-02-29 04:42:50 -0800

    side drawer

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -3,16 +3,17 @@ import React from 'react';
 import UsersList from '../components/UsersList';
 
 const Users = () => {
-  const USERS = [
-    {
-      id: 'u1', 
-      name: 'Filo Tan', 
-      image: 'https://vignette.wikia.nocookie.net/the-rising-of-the-shield-hero/images/9/92/Firo_Light_Novel.png/revision/latest/scale-to-width-down/310?cb=20200202112701',
-      places: 3
-    }
-  ];
+	const USERS = [
+		{
+			id: 'u1',
+			name: 'Filo',
+			image:
+				'https://vignette.wikia.nocookie.net/the-rising-of-the-shield-hero/images/9/92/Firo_Light_Novel.png/revision/latest/scale-to-width-down/310?cb=20200202112701',
+			places: 3
+		}
+	];
 
-  return <UsersList items={USERS}/>;
+	return <UsersList items={USERS} />;
 };
 
 export default Users;
