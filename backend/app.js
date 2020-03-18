d2821b924b6ed31858d51a11d50e1fc1e14ef19d
ntbog <wakasaga@gmail.com>
2020-03-17 22:15:27 -0700

    all api routes

--- backend/app.js
+++ backend/app.js
@@ -2,9 +2,29 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const placesRoutes = require('./routes/places-routes');
+const usersRoutes = require('./routes/users-routes');
+const HttpError = require('./models/http-error');
 
 const app = express();
 
-app.use(placesRoutes); // => /api/places/...
+app.use(bodyParser.json());
+
+app.use('/api/places', placesRoutes); // => /api/places/...
+
+app.use('/api/users', usersRoutes);
+
+// routes that don't get a response at all(because we don't want to respond to it) end up here
+app.use((req, res, next) => {
+	const error = new HttpError('Could not find this route.', 404);
+	throw error;
+});
+
+app.use((error, req, res, next) => {
+	if (res.headerSent) {
+		return next(error);
+	}
+	res.status(error.code || 500);
+	res.json({ message: error.message || 'An unknown error occured!' });
+});
 
 app.listen(5000);
